0e952ee8657f8171b2a44b5a9a363a1fcbf59772
Tim Studt <tstudt@gnresound.com>
2015-04-25 22:44:18 +0200

    Initial Commit

---
+++ TimeTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeTracker
+//
+//  Created by Tim Studt on 25/04/15.
+//  Copyright (c) 2015 Tim Studt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
